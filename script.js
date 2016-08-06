af0986bdff1f820c748cac55cc58cd57b4671705
Naddyson <Naddyson777@gmail.com>
2016-08-06 19:33:26 +0400

    Comments
    Добавил комментарии

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 var main=function(){
+	//автозаполнение контейнера квадратными блоками для разного размера контейнера
 	var elementCount = ($('.workspace').width())*($(".workspace").height())/
 		(Math.pow($('.square').width(),2));
 	for (var i = 0; i < elementCount-1; i++) {
 		var square = $('<div>').addClass('square').appendTo('.workspace');
 		$('<div>').addClass('text').appendTo(square);
 	}
+	//обработчик наведения мыши на квадрат
 	$('.square').mouseenter(function (){
+
 		var color = colorGenerator();
 		(this).style.backgroundColor=color;
 		$(this).children('.text').empty();
 		var tone=determineColorTone(color);
 		
 		$(this).children('.text').text(color);
+		//определяет подходящий цвет текста для квадрата
 		if (tone==='light')
 			$(this).children('.text').css('color','black');
 		else $(this).children('.text').css('color','white');
 	});
 }
-function colorGenerator(){
+function colorGenerator(){//генерирует псевдо-случайный цвет
 	var symbvols="0123456789ABCDEF";
 	var color="#";
 	for (var i = 0; i < 6; i++) {
@@ -25,7 +29,7 @@ function colorGenerator(){
 	}
 	return color;
 }
-function determineColorTone(color){
+function determineColorTone(color){//определяет светлый или темный цвет
 	var R=parseInt(color.substring(1,3),16);
 	var G=parseInt(color.substring(3,5),16);
 	var B=parseInt(color.substring(5,7),16);
@@ -47,12 +51,10 @@ function determineColorTone(color){
 		case 'B': sum=R+G;
 	}
 
-	console.log('color '+color+'R='+R+' G='+G+' B='+B+' sum = '+sum+' main='+main);
+	//console.log('color '+color+'R='+R+' G='+G+' B='+B+' sum = '+sum+' main='+main); для отладки
 	if (sum>255) return 'light'
 		else return 'dark'
 	
 }
 
-determineColorTone('#642FF7');
-determineColorTone('#748686');
 $(document).ready(main);
